85b860be7f511f857774b882e62769dc260f9255
Дмитрий Алексеевич Тихомиров <311435@niuitmo.ru>
2020-11-02 17:30:05 +0300

    new lab

--- 2LabDC_C++/2LabDC_C++/Source.cpp
+++ 2LabDC_C++/2LabDC_C++/Source.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
+#include<algorithm>
 #include<cmath>
 #include"bitmap_image.hpp" // стороняя библиотека, использующаяся для работы с изображениями
 using namespace std;
+
+
+
+
+
+
 
 int main()
 {
@@ -38,6 +45,60 @@ int main()
     for (int i = 0; i < 14; i++)
         ent = ent + (v[i] / 128.0) * log2((v[i] / 128.0));
     cout << -ent;
+
+    sort(v, v + 14);
+    for (int i = 0; i < 14; i++) 
+        cout << v[i] << "\t";
+
+    for (int i = 0; i < 128; i++) // вывод последовательности
+    {
+        switch (pixels[i][63])
+        {
+        case 0:
+            cout << 1111111 << " ";
+            break;
+        case 140:
+            cout << "00" << " ";
+            break;
+        case 160:
+            cout << "110" << " ";
+            break;
+        case 180:
+            cout << "101 ";
+            break;
+        case 200:
+            cout << "011 ";
+            break;
+        case 120:
+            cout << "1111 ";
+            break;
+        case 80:
+            cout << "1001 ";
+            break;
+        case 100:
+            cout << "1000 ";
+            break;
+        case 60:
+            cout << "11101 ";
+            break;
+        case 20:
+            cout << "0100 ";
+            break;
+        case 40:
+            cout << "01011 ";
+            break;
+        case 220:
+            cout << "111001 ";
+            break;
+        case 240:
+            cout << "01010 ";
+            break;
+        case 260:
+            cout << "1110000 ";
+            break;
+        }
+    }
+    cout << endl;
 
 	return 0;
 
